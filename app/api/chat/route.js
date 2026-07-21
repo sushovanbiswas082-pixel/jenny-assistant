2e8aa08c538b70e70c8fcf385a834a573603de98
sushovanbiswas082-pixel <sushovanbiswas082@gmail.com>
2026-07-21 20:05:02 +0530

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
   const { message } = await req.json();
 
   const completion = await client.chat.completions.create({
-    model: "deepseek/deepseek-chat-v3-0324",
+    model: "meta-llama/llama-3.3-8b-instruct:free",
     messages: [{ role: "user", content: message }],
   });
 
